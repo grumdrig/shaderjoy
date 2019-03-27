bb3127f0e617f2f21de697e815c4af5d3c0877d4
Eric Fredricksen <efredricksen@gmail.com>
2019-03-26 17:17:30 -0700

    Doing pretty close to what we want

--- certainpeople.glsl
+++ certainpeople.glsl
@@ -11,9 +11,19 @@ float positive(float v) {
 	return v > 1.0 ? 1.0 : v < 0.0 ? 0.0 : v;
 }
 
+bool test(float v) {
+    int n = int(floor(v));
+    return ((1 & n & n >> 3 & n >> 6 & n >> 9) == 1);
+}
+
 float f(float v) {
-    float scale = iGlobalTime;
-    return positive(min(sin(v * scale), min(sin(v * scale * 8.0), sin(v * scale * 64.0))));
+    float t = iGlobalTime / 4.0;
+    float p = fract(t);
+    float scale = 10.0 * pow(8.0, p);
+    int n = int(floor(v * scale));
+    // p = sqrt(p);
+    return test(v * scale) || test(v * scale + p) || test(v * scale - p) ? 1.0 : 0.0;
+    // return positive(min(sin(v * scale), min(sin(v * scale * 8.0), sin(v * scale * 8.0))));
 }
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
@@ -22,7 +32,14 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     uv = uv * 2. - 1.;
     uv.x *= iResolution.x / iResolution.y;
     uv *= 1.5;
-    float c = max(f(uv.x), f(uv.y));
+    uv += vec2(-2.0, -2.0);
+    float t = iGlobalTime / 20.0;
+    float x0 = sin(33.0 * t) * cos(9.0 * t);
+    float y0 = sin(40.0 * t) * sin(7.0 * t);
+    t /= 10.0;
+    x0 += 2.0 * sin(33.0 * t) * cos(9.0 * t);
+    y0 += 2.0 * sin(40.0 * t) * sin(7.0 * t);
+    float c = max(f(uv.x + x0), f(uv.y + y0));
     fragColor = vec4(c,c,c,1);
 }
 
