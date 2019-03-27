910780f42366dc9f1fceb68bffafa5a97e99d9c5
Eric Fredricksen <efredricksen@gmail.com>
2019-03-26 17:38:53 -0700

    Added a rotation

--- certainpeople.glsl
+++ certainpeople.glsl
@@ -1,29 +1,24 @@
 precision highp float;
 
 
-vec2 rand(float p) {
-    return fract(sin(vec2(p * 12.984 + 23.982,
-                          p * 84.281 + 12.849)) * 43758.5453);
-}
-
-float positive(float v) {
-	v *= 1000.0;
-	return v > 1.0 ? 1.0 : v < 0.0 ? 0.0 : v;
-}
-
 bool test(float v) {
     int n = int(floor(v));
     return ((1 & n & n >> 3 & n >> 6 & n >> 9) == 1);
 }
 
-float f(float v) {
+vec2 rotate(vec2 v, float a){
+    return mat2(cos(a),-sin(a),
+                sin(a),cos(a)) * v;
+}
+
+float f(vec2 v) {
     float t = iGlobalTime / 4.0;
     float p = fract(t);
     float scale = 10.0 * pow(8.0, p);
-    int n = int(floor(v * scale));
-    // p = sqrt(p);
-    return test(v * scale) || test(v * scale + p) || test(v * scale - p) ? 1.0 : 0.0;
-    // return positive(min(sin(v * scale), min(sin(v * scale * 8.0), sin(v * scale * 8.0))));
+    ivec2 n = ivec2(floor(v * scale));
+    vec2 q = vec2(p,p);
+    return max(test(v.x * scale) || test(v.x * scale + q.x) || test(v.x * scale - q.x) ? 1.0 : 0.0,
+               test(v.y * scale) || test(v.y * scale + q.y) || test(v.y * scale - q.y) ? 1.0 : 0.0);
 }
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
@@ -31,15 +26,21 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
 	vec2 uv = fragCoord.xy / iResolution.xy;
     uv = uv * 2. - 1.;
     uv.x *= iResolution.x / iResolution.y;
-    uv *= 1.5;
+    uv *= 2.0;
+
+    float tr = iGlobalTime / 200.0;
+	float r = 3.14159265 * sin(21.0 * tr) * sin(5.0 * tr);
+	uv =rotate(uv, r);
+
     uv += vec2(-2.0, -2.0);
+
     float t = iGlobalTime / 20.0;
     float x0 = sin(33.0 * t) * cos(9.0 * t);
     float y0 = sin(40.0 * t) * sin(7.0 * t);
     t /= 10.0;
     x0 += 2.0 * sin(33.0 * t) * cos(9.0 * t);
     y0 += 2.0 * sin(40.0 * t) * sin(7.0 * t);
-    float c = max(f(uv.x + x0), f(uv.y + y0));
+    float c = f(uv + vec2(x0, y0));
     fragColor = vec4(c,c,c,1);
 }
 
